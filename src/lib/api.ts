cef638eabe91d80e7d0226c93cb9c841af24a293
Rashed99m <mido2k1180@gmail.com>
2024-09-01 01:40:54 +0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://yallanowtripss.runasp.net/api',  // Updated base URL
+    baseURL: 'https://yallanowtripss.runasp.net',  // Updated base URL
 });
 
 api.interceptors.request.use((config) => {
